f0f2566f5e39528187ed8761c38930521589437b
LaneyS05 <laney.staggs05@gmail.com>
2024-08-26 14:53:51 -0400

    while loops

---
+++ Loops/While.py
@@ -0,0 +1,15 @@
+i = 1
+
+while i <= 5:
+    print("Laney ", end="")
+    j=1
+    while j <= 4:
+        print("rock ", end="")
+        j = j + 1
+
+    i = i + 1
+    print()
+
+
+# if you have a nested while loops 
+# the inner loop will complete before the outher loop
